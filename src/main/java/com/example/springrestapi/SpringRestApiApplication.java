229ed687cd74fbf4b7aa5ed44289a1845bf4852d
mohamed-abdulla <abdabdulla78@gmail.com>
2024-01-20 20:49:48 +0530

    neon postgresql db added

--- src/main/java/com/example/springrestapi/SpringRestApiApplication.java
+++ src/main/java/com/example/springrestapi/SpringRestApiApplication.java
@@ -2,7 +2,6 @@ package com.example.springrestapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class SpringRestApiApplication {
 	public static void main(String[] args) {
